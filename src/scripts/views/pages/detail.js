0fa979f63d511cd0d49c26575952303787934ce7
Raisul Amin R <125785637+raicuul42@users.noreply.github.com>
2023-05-02 17:08:38 +0800

    Add files via upload

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,19 @@
+import UrlParser from '../../routes/url-parser';
+import TheMovieDbSource from '../../data/themoviedb-source';
+
+const Detail = {
+  async render() {
+    return `
+      <h2>Detail Page</h2>
+    `;
+  },
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const movie = await TheMovieDbSource.detailMovie(url.id);
+    console.log(movie);
+
+    // TODO: tampilkan movie di dalam DOM
+  },
+};
+
+export default Detail;
---
+++ src/scripts/views/pages/now-playing.js
@@ -0,0 +1,18 @@
+import TheMovieDbSource from '../../data/themoviedb-source';
+
+const NowPlaying = {
+  async render() {
+    return `
+      <h2>Now Playing Page</h2>
+    `;
+  },
+
+  async afterRender() {
+    const movies = await TheMovieDbSource.nowPlayingMovies();
+    console.log(movies);
+
+    // TODO: tampilkan movies di dalam DOM
+  },
+};
+
+export default NowPlaying;
