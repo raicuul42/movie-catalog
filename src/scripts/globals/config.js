520f0bbfd38c47d09870d9db5d81e8835bf1bfe4
Raisul Amin R <125785637+raicuul42@users.noreply.github.com>
2023-05-03 11:00:22 +0800

    Delete src directory

--- src/scripts/globals/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  KEY: 'ca3961fce8cefc02d21a28dd743b2396',
-  BASE_URL: 'https://api.themoviedb.org/3/',
-  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-  DEFAULT_LANGUAGE: 'en-us',
-};
-
-export default CONFIG;
