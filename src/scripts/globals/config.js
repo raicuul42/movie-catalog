141cc49c4b3032a9d21e460c0b9cf49eaea0debc
Raisul Amin R <125785637+raicuul42@users.noreply.github.com>
2023-05-03 14:24:24 +0800

    Add files via upload

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: 'ca3961fce8cefc02d21a28dd743b2396',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+};
+
+export default CONFIG;
